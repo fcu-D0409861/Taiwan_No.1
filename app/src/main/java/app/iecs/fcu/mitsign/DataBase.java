e5b771ce6b202c1841ef3a79447730f5cdfb57c0
Chuan.an <d0486467@mail.fcu.edu.tw>
2017-05-01 21:35:08 +0800

    Version 1.2 By Chuan.an

---
+++ app/src/main/java/app/iecs/fcu/mitsign/DataBase.java
@@ -0,0 +1,97 @@
+package app.iecs.fcu.mitsign;
+
+/* Created by Chuan.an on 2017/5/1. */
+
+public class DataBase {
+    public String FindItem (String digit8,String digit5){
+        String theResult="",theCompany="unknow",theItem="unknow";
+
+        switch(digit8){
+            case "11111111":{
+                theCompany = "一路到底公司";
+                theItem = FindItemSub(digit5);
+                break;
+            }
+            case "22222222":{
+                theCompany = "兩全齊美公司";
+                theItem = FindItemSub(digit5);
+                break;
+            }
+            case "33333333":{
+                theCompany = "三陽開泰公司";
+                theItem = FindItemSub(digit5);
+                break;
+            }
+            case "44444444":{
+                theCompany = "四通八達公司";
+                theItem = FindItemSub(digit5);
+                break;
+            }
+            case "55555555":{
+                theCompany = "五福臨門公司";
+                theItem = FindItemSub(digit5);
+                break;
+            }
+            case "66666666":{
+                theCompany = "六六大順公司";
+                theItem = FindItemSub(digit5);
+                break;
+            }
+            case "77777777":{
+                theCompany = "七步成詩公司";
+                theItem = FindItemSub(digit5);
+                break;
+            }
+            case "88888888":{
+                theCompany = "八扒叭仈公司";
+                theItem = FindItemSub(digit5);
+                break;
+            }
+            case "99999999":{
+                theCompany = "九九九九公司";
+                theItem = FindItemSub(digit5);
+                break;
+            }
+            case "00000000":{
+                theCompany = "零零零零公司";
+                theItem = FindItemSub(digit5);
+                break;
+            }
+            case "12345678":{
+                theCompany = "數字好順公司";
+                theItem = FindItemSub(digit5);
+                break;
+            }
+            case "55688000":{
+                theCompany = "臺灣大車隊";
+                theItem = FindItemSub(digit5);
+                break;
+            }
+            default:{
+
+            }
+        }
+        theResult = theCompany + " " + theItem;
+        return theResult;
+    }
+    private String FindItemSub (String digit5){
+        String theResult="unknow";
+
+        switch(digit5){
+            case "12345":{
+                theResult="高科技智能毛巾";
+                break;
+            }
+            case "55688":{
+                theResult="金屬質感傳輸線";
+                break;
+            }
+            default:{
+                theResult="公司上市紀念筆";
+                break;
+            }
+        }
+
+        return theResult;
+    }
+}
--- app/src/main/java/app/iecs/fcu/mitsign/ResultActivity.java
+++ app/src/main/java/app/iecs/fcu/mitsign/ResultActivity.java
@@ -1,6 +1,7 @@
 package app.iecs.fcu.mitsign;
 
 import android.content.Intent;
+import android.support.v4.media.MediaBrowserServiceCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -16,5 +17,15 @@ public class ResultActivity extends AppCompatActivity {
         String Serial_A = myIntent.getStringExtra("Serial_A");
         String Serial_B = myIntent.getStringExtra("Serial_B");
         // 取得來自MainActivity的兩組號碼 Serial_A & Serial_B
+
+        TextView tv4 = (TextView)findViewById(R.id.textView4);
+        tv4.setText(Serial_A);
+        TextView tv5 = (TextView)findViewById(R.id.textView5);
+        tv5.setText(Serial_B);
+
+        DataBase myBase = new DataBase(); //New DataBase
+        String theResult = myBase.FindItem(Serial_A,Serial_B); //取得結果
+        TextView tv6 = (TextView)findViewById(R.id.textView6);
+        tv6.setText(theResult);
     }
 }
