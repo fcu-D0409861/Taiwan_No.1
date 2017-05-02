c01fd3aa80cbf9adafe37bcf729c5bf8b2231afa
Chuan.an <d0486467@mail.fcu.edu.tw>
2017-05-02 17:49:25 +0800

    Version 1.5 By Chuan.an

--- app/src/main/java/app/iecs/fcu/mitsign/ResultActivity.java
+++ app/src/main/java/app/iecs/fcu/mitsign/ResultActivity.java
@@ -1,31 +1,44 @@
 package app.iecs.fcu.mitsign;
 
 import android.content.Intent;
-import android.support.v4.media.MediaBrowserServiceCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.view.View.OnClickListener;
+
+import org.w3c.dom.Text;
 
 public class ResultActivity extends AppCompatActivity {
+
+    Button getBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
+        getBack = (Button)findViewById(R.id.bt_back);
+        getBack.setOnClickListener(letsBack);
         Intent myIntent = getIntent();
-        String Serial_A = myIntent.getStringExtra("Serial_A");
-        String Serial_B = myIntent.getStringExtra("Serial_B");
+        String Company = myIntent.getStringExtra("Serial_A");
+        String Product = myIntent.getStringExtra("Serial_B");
         // 取得來自MainActivity的兩組號碼 Serial_A & Serial_B
 
-        TextView tv4 = (TextView)findViewById(R.id.textView4);
-        tv4.setText(Serial_A);
-        TextView tv5 = (TextView)findViewById(R.id.textView5);
-        tv5.setText(Serial_B);
+        TextView serial_number = (TextView)findViewById(R.id.tv_result_int);
+        serial_number.setText(Company + " - " + Product);
 
         DataBase myBase = new DataBase(); //New DataBase
-        String theResult = myBase.FindItem(Serial_A,Serial_B); //取得結果
-        TextView tv6 = (TextView)findViewById(R.id.textView6);
-        tv6.setText(theResult);
+        String theResult = myBase.FindItem(Company,Product); //取得結果
+        TextView F_result = (TextView)findViewById(R.id.tv_result);
+        F_result.setText(theResult);
     }
+    private OnClickListener letsBack = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finish();
+        }
+    };
+
 }
